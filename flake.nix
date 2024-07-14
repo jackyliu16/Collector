270c6e2a38cea33423aa2f3e91faeab41579232e
jackyliu16 <18922251299@163.com>
2024-07-14 15:49:15 +0800

    chore(flake): use flake-utils for eachDefaultSystem support

--- flake.nix
+++ flake.nix
@@ -3,13 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self
+    , nixpkgs
+    , flake-utils
+  }@inputs: flake-utils.lib.eachDefaultSystem (system: let 
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.zola = pkgs.zola;
+  });
 }
